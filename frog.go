f5572f8c2497a03dc07424a9fec3d43bee9abf5e
市川恭佑 <github@mail.ebi-yade.com>
2022-05-02 15:19:35 +0900

    Create nil-able func BoolVar

--- frog.go
+++ frog.go
@@ -1,6 +1,27 @@
 package frog
 
-import "flag"
+import (
+	"errors"
+	"flag"
+	"strconv"
+)
+
+type boolValue struct {
+	ptr *bool
+}
+
+func (b *boolValue) String() string {
+	return strconv.FormatBool(*b.ptr)
+}
+
+func (b boolValue) Set(val string) error {
+	res, err := strconv.ParseBool(val)
+	if err != nil {
+		return errors.New("parse error")
+	}
+	b.ptr = &res
+	return nil
+}
 
 // stringValue represents nil-able flag.Value of string
 type stringValue struct {
@@ -14,6 +35,12 @@ func (s *stringValue) Set(val string) error {
 
 func (s *stringValue) String() string {
 	return *s.ptr
+}
+
+// BoolVar defines a bool flag with specified name, and usage string.
+// The argument p points to a string variable in which to store the value of the flag.
+func BoolVar(p *bool, name string, usage string) {
+	flag.CommandLine.Var(&boolValue{ptr: p}, name, usage)
 }
 
 // StringVar defines a string flag with specified name, and usage string.
